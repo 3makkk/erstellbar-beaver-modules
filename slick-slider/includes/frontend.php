ba3b1cdff552743438565dd7326654b237ae7eb8
Sven Friedemann <sven@erstellbar.de>
2016-04-18 18:30:14 +0200

    Improve Slick Slider
    Add action hooks to frontend

--- slick-slider/includes/frontend.php
+++ slick-slider/includes/frontend.php
@@ -11,7 +11,7 @@
  */
 ?>
 
-
+<?php do_action(ERSTELLBAR_SLUG. '_before_slick_slider'); ?>
 <div class="erstellbar-slider" id="<?php echo $id ?>" data-slick="<?php echo $module->getSliderOptions() ?>">
     <?php if ($settings->slide_content_type === 'images'): ?>
         <?php foreach ($settings->images as $image_id): ?>
@@ -19,5 +19,34 @@
                 <?php echo wp_get_attachment_image($image_id, 'large') ?>
             </div>
         <?php endforeach ?>
+    <?php elseif($settings->slide_content_type == 'posts'): ?>
+        <?php $query = FLBuilderLoop::query($settings); ?>
+        <?php if ($query->have_posts()): ?>
+                <?php while ($query->have_posts()): $query->the_post(); ?>
+                    <div class="erstellbar-slides">
+                        <?php do_action(ERSTELLBAR_SLUG. '_before_slick_slider_image'); ?>
+
+                        <?php the_post_thumbnail(); ?>
+
+                        <?php do_action(ERSTELLBAR_SLUG. '_before_slick_slider_title'); ?>
+
+                        <h5 class="erstellbar-slides-title"><?php the_title()?></h5>
+
+                        <?php do_action(ERSTELLBAR_SLUG. '_before_slick_slider_content'); ?>
+
+                        <?php the_content()?>
+
+                        <?php do_action(ERSTELLBAR_SLUG. '_after_slick_slider_content'); ?>
+                    </div>
+                <?php endwhile; ?>
+        <?php endif;
+        wp_reset_postdata(); ?>
+    <?php else: ?>
+        <?php foreach ($settings->custom_slides as $custom_slide): ?>
+            <div class="erstellbar-slides">
+                <?php echo $custom_slide ?>
+            </div>
+        <?php endforeach ?>
     <?php endif; ?>
 </div>
+<?php do_action(ERSTELLBAR_SLUG. '_after_slick_slider'); ?>
